9d35b87787c9d0f2724dbde4ba69ba278569ef6e
James Rostirolla <j_rostirolla@hotmail.com>
2021-07-01 13:19:01 +1000

    text box color mods

---
+++ Develop/script.js
@@ -0,0 +1,41 @@
+let timeDisplay = document.getElementById("currentDay");
+//let currentTimeString = 
+
+//text-block selectors
+let blockColorSix = document.getElementById("six");
+let blockColorSeven = document.getElementById("seven");
+let blockColorEight = document.getElementById("eight");
+let blockColorNine = document.getElementById("nine");
+let blockColorTen = document.getElementById("ten");
+let blockColorEleven = document.getElementById("eleven");
+let blockColorTwelve = document.getElementById("twelve");
+let blockColorThirteen = document.getElementById("thirteen");
+let blockColorFourteen = document.getElementById("fourteen");
+let blockColorFifteen = document.getElementById("fifteen");
+let blockColorSixteen = document.getElementById("sixteen");
+let blockColorSeventeen = document.getElementById("seventeen");
+let blockColorEighteen = document.getElementById("eighteen");
+let blockColorNineteen = document.getElementById("nineteen");
+let blockColorTwenty = document.getElementById("twenty");
+
+//TODO: ask alu to help me fix this
+//let currentTime = moment().format("DD MM YY h:mm:ss a")
+
+
+
+/* 
+timeblock colour change:
+if (currentTime > timeblockTime) {
+
+}
+
+//colour changers:
+if (currentTime < [hourly value of time]) {
+    blockColorSix.className =  "timePast"
+} else if (currentTime == [hourly value of time]) {
+    blockColorSix.className = "timePresent"
+} else if (currentTime > [hourly value of time]) {
+    blockColorSix.className = "timeFuture"
+}
+*/
+
